c9051309b38314ab44c82fd5b621ff3eb3826385
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-06-21 12:06:02 +0900

    コンポーネント間でのデータの受け渡し props v-bind

---
+++ components/HeadingTest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="heading">
+    <h1>{{ heading }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: {
+      type: String,
+      default: 'No Heading',
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+#heading
+  border-bottom: 1px solid #ddd
+  border-top: 1px solid #ddd
+h1
+  color: #f00
+</style>
---
+++ pages/about-test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <heading-test :heading="heading"></heading-test>
+  </div>
+</template>
+<script>
+import HeadingTest from '../components/HeadingTest.vue'
+
+export default {
+  components: {
+    HeadingTest,
+  },
+
+  data() {
+    return {
+      heading: 'hello, Vue.JS',
+    }
+  },
+}
+</script>
